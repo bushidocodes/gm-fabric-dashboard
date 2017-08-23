965bad5369f9769d9e1bd6fdd5740dd74e928f48
Sean McBride <spmcbride1201@gmail.com>
2017-08-23 16:48:35 -0400

    fix: Debounce search to stop polluting history API on keyup (#352)
    * fix: Debounce search to stop polluting history API on keyup
    
    Resolves: 314

--- src/components/Inspector.js
+++ src/components/Inspector.js
@@ -1,49 +1,53 @@
 import { PropTypes } from "prop-types";
-import React from "react";
+import React, { Component } from "react";
+import _ from "lodash";
 
-Inspector.propTypes = {
-  data: PropTypes.array.isRequired,
-  onClick: PropTypes.func.isRequired,
-  onSearch: PropTypes.func.isRequired,
-  searchQuery: PropTypes.string,
-  selectedMetric: PropTypes.string
-};
-export default function Inspector({
-  data,
-  onClick,
-  onSearch,
-  searchQuery,
-  selectedMetric
-}) {
-  const lowerSearchQuery = searchQuery.toLowerCase();
-  const filteredData = searchQuery
-    ? data.filter(i => i.toLowerCase().indexOf(lowerSearchQuery) !== -1)
-    : data;
-  return (
-    <div className="inspector">
-      <div className="inspector-toolbar">
-        <input
-          className="inspector-search"
-          onChange={evt => onSearch(evt.target.value)}
-          placeholder="Search"
-          type="search"
-          value={searchQuery}
-        />
+export default class Inspector extends Component {
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+    onClick: PropTypes.func.isRequired,
+    onSearch: PropTypes.function,
+    searchQuery: PropTypes.string,
+    selectedMetric: PropTypes.string
+  };
+  onSearchDebounced = _.debounce(this.props.onSearch, 1000);
+  onKeyPressHandler = evt => {
+    if (evt.key === "Enter") {
+      this.onSearchDebounced.flush();
+    }
+  };
+  render() {
+    const { data, onClick, searchQuery, selectedMetric } = this.props;
+    const lowerSearchQuery = searchQuery.toLowerCase();
+    const filteredData = searchQuery
+      ? data.filter(i => i.toLowerCase().indexOf(lowerSearchQuery) !== -1)
+      : data;
+    return (
+      <div className="inspector">
+        <div className="inspector-toolbar">
+          <input
+            className="inspector-search"
+            onChange={evt => this.onSearchDebounced(evt.target.value)}
+            onKeyPress={evt => this.onKeyPressHandler(evt)}
+            placeholder="Search"
+            type="search"
+          />
+        </div>
+        {data.length > 0 &&
+          <ul className="inspector-data">
+            {filteredData.map(key =>
+              <li
+                className={`inspector-item ${selectedMetric === key
+                  ? "active"
+                  : ""}`}
+                key={key}
+                onClick={evt => onClick(evt.target)}
+              >
+                {key}
+              </li>
+            )}
+          </ul>}
       </div>
-      {data.length > 0 &&
-        <ul className="inspector-data">
-          {filteredData.map(key =>
-            <li
-              className={`inspector-item ${selectedMetric === key
-                ? "active"
-                : ""}`}
-              key={key}
-              onClick={evt => onClick(evt.target)}
-            >
-              {key}
-            </li>
-          )}
-        </ul>}
-    </div>
-  );
+    );
+  }
 }
