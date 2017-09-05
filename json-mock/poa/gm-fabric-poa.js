ff19b709a1d338e51f0ad288d7c567684b8da57d
Sean McBride <spmcbride1201@gmail.com>
2017-09-05 12:01:23 -0400

    feat: Mock POA service

---
+++ json-mock/poa/gm-fabric-poa.js
@@ -0,0 +1,31 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router();
+const middlewares = jsonServer.defaults();
+const metrics = require("./poa-metrics.json");
+const gometrics = require("./poa-gometrics.json");
+const system = require("./poa-system.json");
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Add custom routes before JSON Server router
+server.get("/metrics", (req, res) => {
+  res.json(metrics);
+});
+server.get("/system", (req, res) => {
+  res.json(system);
+});
+server.get("/gometrics", (req, res) => {
+  res.json(gometrics);
+});
+
+// Use default router
+server.use(router);
+server.listen(9092, () => {
+  console.log("Mocking POA on localhost:9092");
+  console.log("Metrics at /system, /metrics, and /gometrics");
+});
+
+// Issues now that we won't have a backend
+// Golang metrics are not
