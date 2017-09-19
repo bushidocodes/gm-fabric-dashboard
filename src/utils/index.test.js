bd254be1db581662d0b361859138d91b2abd7b86
Sean McBride <spmcbride1201@gmail.com>
2017-09-19 13:48:13 -0400

    chore: Add tests for trimID

---
+++ src/utils/index.test.js
@@ -0,0 +1,18 @@
+import { INSTANCE_ID_LENGTH, trimID } from "./index";
+
+describe("trimID", () => {
+  test("returns an empty string if not provided an ID", () => {
+    expect(trimID()).toEqual("");
+  });
+  test("returns the rightmost possible substring", () => {
+    expect(trimID("Decipher", 3)).toEqual("her");
+  });
+  test("uses INSTNACE_ID_LENGTH when desired length is not provided", () => {
+    expect(trimID("Decipher Tech Studios")).toEqual(
+      trimID("Decipher Tech Studios", INSTANCE_ID_LENGTH)
+    );
+  });
+  test("returns the entire ID when desired length is longer than the length of the actual ID", () => {
+    expect(trimID("Decipher", 20)).toEqual("Decipher");
+  });
+});
