b48d9f82af07744c32393caf31c2ab4c3800b218
Sean McBride <spmcbride1201@gmail.com>
2017-09-21 17:19:45 -0400

    feat: Working Node server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const { promisify } = require("util");
+const fs = require("fs");
+const readFile = promisify(fs.readFile);
+const fabricServer =
+  process.env.FABRIC_SERVER ||
+  "https://edge.deciphernow.com/services/discovery-service/1.0/";
 
 const app = express();
 
@@ -11,12 +17,17 @@ app.use(
   )
 );
 
-// Serve static assets
-app.use(express.static(path.resolve(__dirname, "..", "build")));
+// Serve static assets, and do not automatically direct to the index
+app.use(
+  express.static(path.resolve(__dirname, "..", "build"), { index: false })
+);
 
 // Always return the main index.html, so react-router render the route in the client
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+  readFile(path.resolve(__dirname, "..", "build", "index.html"), "utf8")
+    .then(data => data.replace(/__FABRIC_SERVER__/g, fabricServer))
+    .then(data => res.send(data))
+    .catch(err => console.log(err));
 });
 
 module.exports = app;
