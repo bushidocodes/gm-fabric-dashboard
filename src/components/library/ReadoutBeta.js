0ca6a2117550bd42c11389ff5a98c98a5f06563b
Stuart Hanberg <shanberg@users.noreply.github.com>
2017-10-23 09:31:21 -0400

    Shrinking and centering icons in ReadoutItem component (#660)

--- src/components/library/ReadoutBeta.js
+++ src/components/library/ReadoutBeta.js
@@ -94,6 +94,9 @@ export const ReadoutItemDetail = styled.span`
 
 export const ReadoutItemIcon = styled.div`
   flex: 0 0 ${spacingScale(10)};
-  height: ${spacingScale(10)};
+  height: ${spacingScale(6)};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
 `;
