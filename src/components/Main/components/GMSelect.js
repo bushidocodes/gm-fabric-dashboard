21e3307da6a8f472fb933b0158f44e94ed031ab9
Stuart Hanberg <shanberg@users.noreply.github.com>
2017-10-24 15:03:07 -0400

    feat: Adding z-index mapping to styleVariables and using to fix dropdown layering issue (#683)

--- src/components/Main/components/GMSelect.js
+++ src/components/Main/components/GMSelect.js
@@ -2,6 +2,7 @@ import Select from "react-select";
 import styled from "styled-components";
 // Import the Styles needed by React-Select
 import "react-select/dist/react-select.css";
+import { ZINDEX_DROPDOWN } from "../../../style/styleVariables";
 
 const GMSelect = styled(Select)`
   flex-grow: 1;
@@ -9,6 +10,8 @@ const GMSelect = styled(Select)`
   height: 28px;
   font-weight: 600;
   font-size: 14px;
+  z-index: ${ZINDEX_DROPDOWN};
+  position: relative;
   font-family: "Nunito", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
     "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
