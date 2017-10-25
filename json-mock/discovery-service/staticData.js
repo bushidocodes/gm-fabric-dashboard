594476191cab1beb8a3a162835b9a843bcc41d24
karinakeen <karina.yang@deciphernow.com>
2017-10-25 17:17:34 -0400

    655 restore disabled ajax unit test (#707)
    * chore:  add moxios to project dictionary
    
    * chore: create static array of mock services for ajax unit test
    
    * test: refactor and enable ajax test  "AJAX Web Worker fetches services from the discovery service and maps them with a key of name" to work with static array of mock services

---
+++ json-mock/discovery-service/staticData.js
@@ -0,0 +1,48 @@
+const staticServices = [
+  {
+    name: "Internet Option",
+    version: "3.6",
+    owner: "Sequential",
+    capability: "Crime Fighting",
+    minimum: 5,
+    maximum: 4,
+    documentation: "https://www.google.com",
+    authorized: true,
+    metered: true,
+    threaded: true,
+    runtime: "GO",
+    instances: ["2oajqscqqwu0000000000", "3s50frjd0wg0000000000"]
+  },
+  {
+    name: "Export Team Gateway Up2 Management Message Resource Measurement",
+    version: "1",
+    owner: "Virtual",
+    capability: "Crime Fighting",
+    minimum: 2,
+    maximum: 0,
+    documentation: "https://www.google.com",
+    authorized: true,
+    metered: true,
+    threaded: true,
+    runtime: "GO",
+    instances: ["2l39cmzi8xw0000000000", "36lbqne4i7s0000000000"]
+  },
+  {
+    name: "Up2 Message Network Team Entry Job Transmission End",
+    version: "4.7",
+    owner: "Internet",
+    capability: "Crime Fighting",
+    minimum: 3,
+    maximum: 0,
+    documentation: "https://www.google.com",
+    authorized: true,
+    metered: true,
+    threaded: true,
+    runtime: "GO",
+    instances: ["7clfu88ur9g0000000000"]
+  }
+];
+
+module.exports = {
+  staticServices
+};
