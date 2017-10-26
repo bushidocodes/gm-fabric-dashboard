1b8a66a550ec165cc29d83f7be18c8bed35c5c44
Jamie Sweetland <jamie.sweetland@deciphernow.com>
2017-10-26 13:29:09 -0400

    Now storing data as objects to reduce hits to the DOM, added navigation steps to account for dashboard workflow, and flagged a test step as failing due to a known issue.

--- test-automation/src/main/java/com/blackbox/siteSpecific/tests/pageValidations/SummaryValidations.java
+++ test-automation/src/main/java/com/blackbox/siteSpecific/tests/pageValidations/SummaryValidations.java
@@ -10,30 +10,60 @@ import javax.sound.midi.SysexMessage;
 public class SummaryValidations extends GMFDashboardTest {
     @Test
     public void validateSummaryPageValues() {
+        // Set up data
+        String serviceName = "Network Internet Information";
+        int instanceIndex = 1;
+
+        String uptimeValue;
+        String uptimeDateAndTime;
+        String sidebarUptimeValue;
+        String avgResponseTime;
+        String errorRate;
+        String cores;
+        String host;
+        String port;
+
+
         // Open the site
         gmfDashboardSite.openSite(deployment);
+        gmfDashboardSite.dashboard().waitForPageToLoad();
+
+        // Navigate to the desired service
+        gmfDashboardSite.dashboard().navigateToMainStableServiceEntry(serviceName);
+        gmfDashboardSite.instances().waitForPageToLoad();
+
+        // Navigate to the desired instance and verify the Summary page is loaded
+        gmfDashboardSite.instances().navigateToInstance(instanceIndex);
         gmfDashboardSite.summary().waitForPageToLoad();
 
         // Verify and display the Uptime values
         Assert.assertTrue(gmfDashboardSite.summary().isUptimeValueShown());
         Assert.assertTrue(gmfDashboardSite.summary().isUptimeDateTimeShown());
-        System.out.println(String.format("Uptime: %s (%s)", gmfDashboardSite.summary().getUptimeValue(), gmfDashboardSite.summary().getUptimeDateTime()));
+        uptimeValue = gmfDashboardSite.summary().getUptimeValue();
+        uptimeDateAndTime = gmfDashboardSite.summary().getUptimeDateTime();
+        System.out.println(String.format("Uptime: %s (%s)", uptimeValue, uptimeDateAndTime));
 
         // Verify the uptime value on the page matches the uptime value in the sidebar
-        Assert.assertTrue(gmfDashboardSite.summary().getUptimeValue().equals(gmfDashboardSite.summary().getSidebarUptimeValue()));
+        sidebarUptimeValue = gmfDashboardSite.summary().getSidebarUptimeValue();
+        Assert.assertTrue(uptimeValue.equals(sidebarUptimeValue));  // FIXME: Issue #714
 
         // Verify and display the Avg. Response Time and Error Rate
         Assert.assertTrue(gmfDashboardSite.summary().isAvgResponseTimeShown());
         Assert.assertTrue(gmfDashboardSite.summary().isErrorRateShown());
-        System.out.println(String.format("Avg. Response Time: %s, Error Rate: %s", gmfDashboardSite.summary().getAvgResponseTime(), gmfDashboardSite.summary().getErrorRate()));
+        avgResponseTime = gmfDashboardSite.summary().getAvgResponseTime();
+        errorRate = gmfDashboardSite.summary().getErrorRate();
+        System.out.println(String.format("Avg. Response Time: %s, Error Rate: %s", avgResponseTime, errorRate));
 
         // Verify and display the Cores
         Assert.assertTrue(gmfDashboardSite.summary().isCoresShown());
-        System.out.println(String.format("Cores: %s", gmfDashboardSite.summary().getCores()));
+        cores = gmfDashboardSite.summary().getCores();
+        System.out.println(String.format("Cores: %s", cores));
 
         // Verify and display the Host and Port
         Assert.assertTrue(gmfDashboardSite.summary().isHostShown());
         Assert.assertTrue(gmfDashboardSite.summary().isPortShown());
-        System.out.println(String.format("Host & Port: %s:%s", gmfDashboardSite.summary().getHost(), gmfDashboardSite.summary().getPort()));
+        host = gmfDashboardSite.summary().getHost();
+        port = gmfDashboardSite.summary().getPort();
+        System.out.println(String.format("Host & Port: %s:%s", host, port));
     }
 }
