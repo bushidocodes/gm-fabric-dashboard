ba66d5dbdae50886b23ab4ab98b45bd2f404a350
Jamie Sweetland <jamie.sweetland@deciphernow.com>
2017-10-26 11:47:07 -0400

    Added element locators and action methods to the Instances page.

--- test-automation/src/main/java/com/blackbox/siteSpecific/framework/pages/InstancesPage.java
+++ test-automation/src/main/java/com/blackbox/siteSpecific/framework/pages/InstancesPage.java
@@ -10,6 +10,22 @@ public class InstancesPage extends GMFDashboardPage {
 
     // <editor-fold desc="Sidebar Elements">
 
+    private static final String LINK_BACK_TO_DASHBOARD = "domclass=nav-go-up";
+
+    // </editor-fold>
+
+
+    // <editor-fold desc="Main Page Elements">
+
+    private static final String FIELD_SEARCH_INSTANCES = "domclass=form-control";
+
+    private static final String[] TABLE_ROW_INSTANCE_SUBSTRINGS = new String[]{"//*[@id=\"main-content\"]/div/div[2]/ol/li[", "]"};
+    private static final String[] IMAGE_INSTANCE_STATE_SUBSTRINGS = new String[]{"//*[@id=\"main-content\"]/div/div[2]/ol/li[", "]/div[1]/img"};
+    private static final String[] LINK_INSTANCE_ID_SUBSTRINGS = new String[]{"//*[@id=\"main-content\"]/div/div[2]/ol/li[", "]/div[2]/a"};
+    private static final String[] TEXT_INSTANCE_REQUESTS_SUBSTRINGS = new String[]{"//*[@id=\"main-content\"]/div/div[2]/ol/li[", "]/div[3]"};
+    private static final String[] TEXT_INSTANCE_ERROR_SUBSTRINGS = new String[]{"//*[@id=\"main-content\"]/div/div[2]/ol/li[", "]/div[4]"};
+    private static final String[] TEXT_INSTANCE_UPTIME_SUBSTRINGS = new String[]{"//*[@id=\"main-content\"]/div/div[2]/ol/li[", "]/div[5]"};
+
     // </editor-fold>
 
 
@@ -25,17 +41,49 @@ public class InstancesPage extends GMFDashboardPage {
 
     // <editor-fold desc="Wait for Page to Load">
 
-    public void waitForPageToLoad() {}  // TODO
+    public void waitForPageToLoad() {
+        driverutil.waitForVisibleElement(FIELD_SEARCH_INSTANCES, 60);
+    }
 
     // </editor-fold>
 
 
     // <editor-fold desc="Sidebar Actions">
 
+    public DashboardPage navigateToDashboard() {
+        driverutil.click(LINK_BACK_TO_DASHBOARD);
+        return webSite.setCurrentPage(DashboardPage.class);
+    }
+
     // </editor-fold>
 
 
-    // <editor-fold desc="Main Section">
+    // <editor-fold desc="Main Page Actions">
+
+    public boolean doesInstanceExist(int index) {
+        return driverutil.doesElementExist(buildElementLocator(TABLE_ROW_INSTANCE_SUBSTRINGS, index));
+    }
+
+    public String getInstanceId(int index) {
+        return driverutil.getText(buildElementLocator(LINK_INSTANCE_ID_SUBSTRINGS, index));
+    }
+
+    public String getInstanceRequests(int index) {
+        return driverutil.getText(buildElementLocator(TEXT_INSTANCE_REQUESTS_SUBSTRINGS, index));
+    }
+
+    public String getInstanceError(int index) {
+        return driverutil.getText(buildElementLocator(TEXT_INSTANCE_ERROR_SUBSTRINGS, index));
+    }
+
+    public String getInstanceUptime(int index) {
+        return driverutil.getText(buildElementLocator(TEXT_INSTANCE_UPTIME_SUBSTRINGS, index));
+    }
+
+    public SummaryPage navigateToInstance(int index) {
+        driverutil.click(buildElementLocator(LINK_INSTANCE_ID_SUBSTRINGS, index));
+        return webSite.setCurrentPage(SummaryPage.class);
+    }
 
     // </editor-fold>
 }
