6ca8326056d92ca9962a706d59a21e067d9f8fce
Jamie Sweetland <jamie.sweetland@deciphernow.com>
2017-10-26 12:42:06 -0400

    Added header element locators and visibility checks to the waitForPageToLoad() method.

--- test-automation/src/main/java/com/blackbox/siteSpecific/framework/pages/JvmPage.java
+++ test-automation/src/main/java/com/blackbox/siteSpecific/framework/pages/JvmPage.java
@@ -10,6 +10,9 @@ public class JvmPage extends GMFDashboardServicePage {
 
     // <editor-fold desc="Page Elements">
 
+    private static final String HEADER_HEAP = "//*[@id=\"main-content\"]/div/div[1]/div/h3";
+    private static final String HEADER_CLASSES = "//*[@id=\"main-content\"]/div/div[2]/div/h3";
+
     // </editor-fold>
 
 
@@ -26,7 +29,8 @@ public class JvmPage extends GMFDashboardServicePage {
     // <editor-fold desc="Wait for Page to Load">
 
     public void waitForPageToLoad() {
-        
+        driverutil.waitForVisibleElement(HEADER_HEAP, 60);
+        driverutil.waitForVisibleElement(HEADER_CLASSES, 60);
     }
 
     // </editor-fold>
