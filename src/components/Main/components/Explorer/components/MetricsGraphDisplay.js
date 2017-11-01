ed6cfed79ee7a37fbfc56087a8a811e83b58e3a0
Stuart Hanberg <shanberg@users.noreply.github.com>
2017-11-01 16:58:58 -0400

    feat: Cleaned up Explorer style to better match app (#802)
    * feat: Cleaned up Explorer style to better match app
    
    * fix: Clean up unused variables

--- src/components/Main/components/Explorer/components/MetricsGraphDisplay.js
+++ src/components/Main/components/Explorer/components/MetricsGraphDisplay.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
-import { contrastColor } from "style/styleFunctions";
-import { COLOR_CONTENT_BACKGROUND } from "style/styleVariables";
+import { spacingScale } from "style/styleFunctions";
 
 const MetricsGraphDisplay = styled.div`
-  background-color: ${contrastColor(COLOR_CONTENT_BACKGROUND, 0.04).string()};
   flex: 1 1 100%;
   position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   @media all and (min-width: 1200px) {
-    flex: 0 0 60%;
+    flex: 1 0 60%;
   }
   .chart {
     display: flex;
@@ -24,14 +26,14 @@ const MetricsGraphDisplay = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
-    padding: spacingScale(2);
+    padding: ${spacingScale(2)};
     h3 {
       flex: 0 0 auto;
       word-break: break-all;
       white-space: normal;
       max-width: 100%;
       text-overflow: ellipsis;
-      margin: spacingScale(1);
+      margin: ${spacingScale(1)};
     }
   }
   p {
--- src/components/Main/components/Explorer/components/MetricsList.js
+++ src/components/Main/components/Explorer/components/MetricsList.js
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
-import { contrastColor, spacingScale } from "style/styleFunctions";
+import { edgeColor, spacingScale } from "style/styleFunctions";
 import { COLOR_CONTENT_BACKGROUND } from "style/styleVariables";
 
 const MetricsList = styled.div`
-  background-color: ${contrastColor(COLOR_CONTENT_BACKGROUND, 0.04).string()};
   flex: 0 0 300px;
+  border-right: 1px solid ${edgeColor(COLOR_CONTENT_BACKGROUND).string()};
   position: relative;
   margin-bottom: ${spacingScale(2)};
   @media all and (min-width: 1200px) {
     flex: 0 0 40%;
+    max-width: 550px;
     margin-right: ${spacingScale(2)};
     margin-bottom: 0;
   }
