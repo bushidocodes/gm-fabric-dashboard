77bef3696e47d4638e28426ec03354dc14a56693
Stuart Hanberg <shanberg@users.noreply.github.com>
2017-11-01 18:06:02 -0400

    fix: Fixing dropdown underlap on Fabric Table view (#810)

--- src/style/styleVariables.js
+++ src/style/styleVariables.js
@@ -24,8 +24,8 @@ export const COLOR_WARNING = Color("#FAC60F");
 export const COLOR_INFO = Color("blue");
 
 // Z-Index Mapping
-export const ZINDEX_DROPDOWN = "1000";
-export const ZINDEX_STICKY = "1020";
+export const ZINDEX_STICKY = "1010";
+export const ZINDEX_DROPDOWN = "1020";
 export const ZINDEX_FIXED = "1030";
 export const ZINDEX_MODAL_BACKDROP = "1040";
 export const ZINDEX_MODAL = "1050";
