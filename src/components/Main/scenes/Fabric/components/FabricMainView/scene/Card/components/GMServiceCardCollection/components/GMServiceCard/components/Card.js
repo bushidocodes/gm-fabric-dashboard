3d8a8b281da46c32717b50b6f338a6b30750fe80
Stuart Hanberg <shanberg@users.noreply.github.com>
2017-11-02 15:46:35 -0400

    feat: Rewriting cards layout method to improve spacing and responsiveness (#815)

--- src/components/Main/scenes/Fabric/components/FabricMainView/scene/Card/components/GMServiceCardCollection/components/GMServiceCard/components/Card.js
+++ src/components/Main/scenes/Fabric/components/FabricMainView/scene/Card/components/GMServiceCardCollection/components/GMServiceCard/components/Card.js
@@ -3,15 +3,17 @@ import { PADDING_BASE, FONT_SIZE_SM } from "style/styleVariables";
 import Color from "color";
 import styled from "styled-components";
 
+const CARD_SPACING = PADDING_BASE;
+
 export const CardContainer = styled.div`
   color: ${props => props.cardFontColor};
   background-color: ${props => Color(props.cardBackgroundColor).string()};
   border: 1px solid ${props => props.cardBorderColor};
   border-top: 2px solid ${props => props.cardBorderAltColor};
-  width: ${props => props.width};
+  width: 100%;
   border-radius: 1px;
   height: ${props => props.height};
-  margin: ${parseInt(PADDING_BASE, 10) / 2}px;
+  margin: ${parseInt(CARD_SPACING, 10) / 2}px;
   padding: ${parseInt(PADDING_BASE, 10) * 1.5}px
     ${parseInt(PADDING_BASE, 10) * 1.5}px ${parseInt(PADDING_BASE, 10)}px;
   font-size: ${FONT_SIZE_SM};
@@ -19,6 +21,25 @@ export const CardContainer = styled.div`
   flex-direction: column;
   justify-content: space-between;
   position: relative;
+
+  @media all and (min-width: 375px) {
+    width: calc((100% / 2) - ${CARD_SPACING});
+  }
+  @media all and (min-width: 530px) {
+    width: calc((100% / 3) - ${CARD_SPACING});
+  }
+  @media all and (min-width: 704px) {
+    width: calc((100% / 4) - ${CARD_SPACING});
+  }
+  @media all and (min-width: 875px) {
+    width: calc((100% / 5) - ${CARD_SPACING});
+  }
+  @media all and (min-width: 1040px) {
+    width: calc((100% / 6) - ${CARD_SPACING});
+  }
+  @media all and (min-width: 1208px) {
+    width: calc((100% / 7) - ${CARD_SPACING});
+  }
 `;
 
 export const CardFooter = styled.div`
