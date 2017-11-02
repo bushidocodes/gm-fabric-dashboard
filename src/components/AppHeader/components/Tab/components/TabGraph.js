c896eab01b4eb8f95519ac13f16f64c0fc1a7c93
Kait Moreno <kaitlin.moreno@deciphernow.com>
2017-11-02 12:30:21 -0400

    Fix: Sparklines overflow on Tab (#806)
    * fix: removed height from TabGraph
    
    * fix:  sparklines positioning
    
    * fix: Forcing Sparklines to fill allocated space

--- src/components/AppHeader/components/Tab/components/TabGraph.js
+++ src/components/AppHeader/components/Tab/components/TabGraph.js
@@ -9,9 +9,13 @@ import { COLOR_ALT_BACKGROUND } from "../../../../../style/styleVariables";
 const COLOR_TAB_BACKGROUND_BASE = contrastColor(COLOR_ALT_BACKGROUND, 0.175);
 
 const TabGraph = styled.div`
+  height: ${spacingScale(3)};
   color: ${contrastColor(COLOR_TAB_BACKGROUND_BASE, 0.8).string()};
   margin: 0 ${spacingScale(0.25)};
-  height: ${spacingScale(3)};
+  > * {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 export default TabGraph;
