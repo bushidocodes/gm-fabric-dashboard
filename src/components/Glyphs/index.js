fb0bbb8189448114fdf4128f08f80a5b8a80ec09
Martin Terskin <martin@martintersk.in>
2017-11-09 16:31:46 -0500

    refactor: converted old svg glyphs to new reusable format (#935)
    * refactor: converted old svg glyphs to new reusable format
    
    * implementing feedback
    
    * chore: alphabetize glyph list

--- src/components/Glyphs/index.js
+++ src/components/Glyphs/index.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { PropTypes } from "prop-types";
 import _ from "lodash";
 
+import Bars from "./Bars";
+import Bell from "./Bell";
 import CPU from "./CPU";
 import Card from "./Card";
 import Close from "./Close";
@@ -9,13 +11,18 @@ import Cog from "./Cog";
 import Configuration from "./Configuration";
 import Docs from "./Docs";
 import EKG from "./EKG";
+import EditGraph from "./EditGraph";
+import ErrorList from "./ErrorList";
 import Exclamation from "./Exclamation";
 import Explorer from "./Explorer";
 import Fabric from "./Fabric";
 import Finagle from "./Finagle";
 import Functions from "./Functions";
+import GRPC from "./GRPC";
 import GitHub from "./GitHub";
 import Http from "./Http";
+import Info from "./Info";
+import Instances from "./Instances";
 import JVM from "./JVM";
 import Key from "./Key";
 import LinkedIn from "./LinkedIn";
@@ -25,21 +32,30 @@ import Negation from "./Negation";
 import NoKey from "./NoKey";
 import NoMetrics from "./NoMetrics";
 import Pause from "./Pause";
+import Person from "./Person";
 import Play from "./Play";
 import Poll from "./Poll";
 import Power from "./Power";
+import Rows from "./Rows";
 import RunningSmall from "./RunningSmall";
+import Scale from "./Scale";
 import Scatterplot from "./Scatterplot";
 import Service from "./Service";
 import ServiceInstance from "./ServiceInstance";
+import ServicesWhite from "./ServicesWhite";
+import StarFilled from "./StarFilled";
 import Summary from "./Summary";
 import Tape from "./Tape";
 import Threads from "./Threads";
 import Timer from "./Timer";
 import Twitter from "./Twitter";
+import ViewCollapse from "./ViewCollapse";
 
 // import all glyph components
+
 const glyphs = {
+  Bars,
+  Bell,
   CPU,
   Card,
   Close,
@@ -47,14 +63,19 @@ const glyphs = {
   Configuration,
   Docs,
   EKG,
+  EditGraph,
+  ErrorList,
   Exclamation,
   Explorer,
   Fabric,
   Finagle,
   Functions,
+  GRPC,
   GitHub,
   Go: JVM,
   Http,
+  Info,
+  Instances,
   JVM,
   Key,
   LinkedIn,
@@ -64,18 +85,24 @@ const glyphs = {
   NoKey,
   NoMetrics,
   Pause,
+  Person,
   Play,
   Poll,
   Power,
+  Rows,
   RunningSmall,
+  Scale,
   Scatterplot,
   Service,
   ServiceInstance,
+  ServicesWhite,
+  StarFilled,
   Summary,
   Tape,
   Threads,
   Timer,
-  Twitter
+  Twitter,
+  ViewCollapse
 };
 
 /**
