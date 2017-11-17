dd5329689dd749be57e4f52a97af31e80825601e
Joshua Mark Rutherford <joshua.rutherford@deciphernow.com>
2017-11-17 15:24:19 -0500

    CHANGES

--- api-tests/src/main/java/com/blackbox/siteSpecific/tests/api/RestApiTest.java
+++ api-tests/src/main/java/com/blackbox/siteSpecific/tests/api/RestApiTest.java
@@ -1,21 +1,49 @@
 package com.blackbox.siteSpecific.tests.api;
 
 import com.blackbox.siteSpecific.framework.base.ApiTest;
-import com.google.gson.JsonElement;
 import org.junit.Test;
 
+import com.blackbox.common.api.Https;
+
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.TrustManager;
+import java.net.URL;
+import java.security.KeyStore;
 
 public class RestApiTest extends ApiTest {
+
+    String SERVICE_URL = "https://edge.deciphernow.com/services/discovery-service/1.0/services";
+    String KEYSTORE_FILE = "/Users/joshua.rutherford/keystore.jks";
+    String KEYSTORE_PASSWORD = "password";
+    String TRUSTSTORE_FILE = "/Users/joshua.rutherford/truststore.jks";
+    String TRUSTSTORE_PASSWORD = "password";
+
     @Test
-    public void getListOfServices() {
-        // Set up objects
-        String restUrl = "https://edge.deciphernow.com/services/discovery-service/1.0/services";
-        JsonElement restResponse;
+    public void getListOfServices() throws Exception {
 
-        // Make the REST call and gather the response
-        restResponse = restUtil.get(restUrl);
+        URL url = new URL(SERVICE_URL);
 
-        // Display the JSON contents
-        System.out.println(restResponse);
+        KeyStore keyStore = Https.loadKeyStore(KEYSTORE_FILE, KEYSTORE_PASSWORD);
+        KeyStore trustStore = Https.loadKeyStore(TRUSTSTORE_FILE, TRUSTSTORE_PASSWORD);
+
+        KeyManager[] keyManagers = Https.loadKeyManagers(keyStore, KEYSTORE_PASSWORD);
+        TrustManager[] trustManagers = Https.loadTrustManager(trustStore);
+
+        try {
+            String response = Https.fetch(keyManagers, trustManagers, url);
+            System.out.println(response);
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+
+//        // Set up objects
+//        String restUrl = "https://edge.deciphernow.com/services/discovery-service/1.0/services";
+//        JsonElement restResponse;
+//
+//        // Make the REST call and gather the response
+//        restResponse = restUtil.get(restUrl);
+//
+//        // Display the JSON contents
+//        System.out.println(restResponse);
     }
 }
