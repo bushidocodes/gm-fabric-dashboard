c36cc373b8ff9e8c064e91e9ffa0771f54b8503c
Jamie Sweetland <jamie.sweetland@deciphernow.com>
2017-11-20 16:18:54 -0500

    Added steps to make sure at least one service is present in the /services response.

--- api-tests/src/main/java/com/blackbox/siteSpecific/tests/api/DiscoveryServicesTests.java
+++ api-tests/src/main/java/com/blackbox/siteSpecific/tests/api/DiscoveryServicesTests.java
@@ -1,11 +1,11 @@
 package com.blackbox.siteSpecific.tests.api;
 
-import com.blackbox.dataModels.ServiceInstance;
+import com.blackbox.dataModels.ServiceModel;
 import com.blackbox.siteSpecific.framework.base.ApiTest;
 import com.blackbox.siteSpecific.framework.services.DiscoveryService;
-import com.google.gson.JsonElement;
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.ArrayList;
 
 public class DiscoveryServicesTests extends ApiTest {
     @Test
@@ -18,10 +18,10 @@ public class DiscoveryServicesTests extends ApiTest {
         // Get the list of available services
         discoveryService.getServices();
 
+        // Make sure at least one service is present
+        Assert.assertTrue(discoveryService.getServiceCount() > 0);
+
         // Validate the format of the JSON response
         Assert.assertTrue(discoveryService.isServicesResponseFormatValid());
-
-        // Get details for the instance of a service
-
     }
 }
