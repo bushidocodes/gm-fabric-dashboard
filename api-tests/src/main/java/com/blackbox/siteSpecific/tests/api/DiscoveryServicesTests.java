2c61652794941e5682e5b9e93522535a55d86f83
Jamie Sweetland <jamie.sweetland@deciphernow.com>
2017-11-20 12:27:23 -0500

    Cleaned up the DiscoveryServicesTests.happyPaths test to handle the new paradigm

--- api-tests/src/main/java/com/blackbox/siteSpecific/tests/api/DiscoveryServicesTests.java
+++ api-tests/src/main/java/com/blackbox/siteSpecific/tests/api/DiscoveryServicesTests.java
@@ -1,16 +1,11 @@
 package com.blackbox.siteSpecific.tests.api;
 
+import com.blackbox.dataModels.ServiceInstance;
 import com.blackbox.siteSpecific.framework.base.ApiTest;
 import com.blackbox.siteSpecific.framework.services.DiscoveryService;
 import com.google.gson.JsonElement;
+import org.junit.Assert;
 import org.junit.Test;
-
-import com.blackbox.common.api.Https;
-
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.TrustManager;
-import java.net.URL;
-import java.security.KeyStore;
 
 public class DiscoveryServicesTests extends ApiTest {
     @Test
@@ -18,8 +13,15 @@ public class DiscoveryServicesTests extends ApiTest {
         // Set up objects
         DiscoveryService discoveryService = new DiscoveryService(deployment);
         JsonElement response;
+        ServiceInstance serviceInstance;
 
         // Get the list of available services
-        response = discoveryService.getServices();
+        discoveryService.getServices();
+
+        // Validate the format of the JSON response
+        Assert.assertTrue(discoveryService.isServicesResponseFormatValid());
+
+        // Get details for the instance of a service
+
     }
 }
