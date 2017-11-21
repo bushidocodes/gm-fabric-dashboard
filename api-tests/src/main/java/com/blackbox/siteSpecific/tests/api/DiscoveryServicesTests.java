f428dbee4bebe9140e68ce874ae559b8e2025d7a
Jamie Sweetland <jamie.sweetland@deciphernow.com>
2017-11-21 13:28:20 -0500

    Added the negativeTests test class to verify that certain REST calls fail as expected.

--- api-tests/src/main/java/com/blackbox/siteSpecific/tests/api/DiscoveryServicesTests.java
+++ api-tests/src/main/java/com/blackbox/siteSpecific/tests/api/DiscoveryServicesTests.java
@@ -5,14 +5,22 @@ import com.blackbox.siteSpecific.framework.base.ApiTest;
 import com.blackbox.siteSpecific.framework.services.DiscoveryService;
 import org.junit.Assert;
 import org.junit.Test;
+
 import java.util.ArrayList;
 
 public class DiscoveryServicesTests extends ApiTest {
+    private ArrayList<ServiceModel> services;
+    private final int serviceIndex = 0;
+    private final int serviceInstanceIndex = 0;
+
+    private final String NONEXISTENT_SERVICE_NAME = "Bananagrams";
+    private final String NONEXISTENT_SERVICE_VERSION = "0.0.0";
+    private final String NONEXISTENT_SERVICE_INSTANCE = "abcdefghijklmnopqrstuvwxyz0123456789";
+
     @Test
     public void happyPaths() {
         // Set up objects
         DiscoveryService discoveryService = new DiscoveryService(deployment);
-        ArrayList<ServiceModel> services;
 
         // Get the list of available services
         discoveryService.getServices();
@@ -20,10 +28,69 @@ public class DiscoveryServicesTests extends ApiTest {
         // Make sure at least one service is present
         Assert.assertTrue(discoveryService.getServiceCount() > 0);
 
-        // Validate the format of the JSON response
+        // Validate the format of the /services response
         Assert.assertTrue(discoveryService.isServicesResponseFormatValid());
 
         // Model the services from the response
-        services = discoveryService.modelServices();
+        this.services = discoveryService.modelServices();
+
+        // TODO: Get metrics for a service
+
+        // TODO: Validate the format of the /metrics response
+
+        // TODO: Get threads for a service
+
+        // TODO: Validate the format of the /threads response
+    }
+
+
+    @Test
+    public void negativeTests() {
+        // Set up objects
+        DiscoveryService discoveryService = new DiscoveryService(deployment);
+        boolean servicesEndpointExceptionReceived = false;
+
+        // Get and model the services if they have not already been modeled
+        if(services == null) {
+            discoveryService.getServices();
+            Assert.assertTrue(discoveryService.getServiceCount() > 0);
+            services = discoveryService.modelServices();
+        }
+
+        // Hit an invalid endpoint based on the /services endpoint and verify the call fails
+        discoveryService.getUrlExpectFailure(
+                discoveryService.getServicesUrl(),
+                services.get(serviceIndex).getUrlName());
+        Assert.assertFalse(discoveryService.didLastRequestSucceed());
+
+        // Hit the /metrics endpoint with parameters for a nonexistent service and verify the call fails
+        discoveryService.getUrlExpectFailure(
+                discoveryService.getMetricsUrl(),
+                new String[]{
+                        NONEXISTENT_SERVICE_NAME,
+                        NONEXISTENT_SERVICE_VERSION,
+                        NONEXISTENT_SERVICE_INSTANCE
+                }
+        );
+        Assert.assertFalse(discoveryService.didLastRequestSucceed());
+
+        // Hit the /metrics endpoint without parameters and verify the call fails
+        discoveryService.getUrlExpectFailure(discoveryService.getMetricsUrl());
+        Assert.assertFalse(discoveryService.didLastRequestSucceed());
+
+        // Hit the /threads endpoint with parameters for a nonexistent service and verify the call fails
+        discoveryService.getUrlExpectFailure(
+                discoveryService.getThreadsUrl(),
+                new String[]{
+                        NONEXISTENT_SERVICE_NAME,
+                        NONEXISTENT_SERVICE_VERSION,
+                        NONEXISTENT_SERVICE_INSTANCE
+                }
+        );
+        Assert.assertFalse(discoveryService.didLastRequestSucceed());
+
+        // Hit the /threads endpoint without parameters and verify the call fails
+        discoveryService.getUrlExpectFailure(discoveryService.getThreadsUrl());
+        Assert.assertFalse(discoveryService.didLastRequestSucceed());
     }
 }
