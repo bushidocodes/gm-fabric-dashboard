8f9af949972f3a0f97983b265cd85f8cbb18b421
Karina Yang <karina.yang@deciphernow.com>
2017-11-28 12:33:35 -0500

    1102 fix: explorer chart wrapper (#1103)
    * fix: explorer chart wrapper
    
    * clean code
    
    * update snapshot

--- src/components/Main/components/Explorer/components/MetricsGraphDisplay.js
+++ src/components/Main/components/Explorer/components/MetricsGraphDisplay.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
-import { spacingScale } from "style/styleFunctions";
-
 const MetricsGraphDisplay = styled.div`
   flex: 1 1 100%;
   position: relative;
@@ -11,36 +9,6 @@ const MetricsGraphDisplay = styled.div`
 
   @media all and (min-width: 800px) {
     flex: 1 0 60%;
-  }
-  .chart {
-    display: flex;
-    align-items: stretch;
-    .chart-title {
-      flex: 0 0 auto;
-    }
-    .chart-content {
-      flex: 1 1 100%;
-      position: relative;
-    }
-  }
-  > * {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    padding: ${spacingScale(2)};
-    h3 {
-      flex: 0 0 auto;
-      word-break: break-all;
-      white-space: normal;
-      max-width: 100%;
-      text-overflow: ellipsis;
-      margin: ${spacingScale(1)};
-    }
-  }
-  p {
-    text-align: center;
-    opacity: 0.8;
-    flex: 1 1 100%;
   }
 `;
 
