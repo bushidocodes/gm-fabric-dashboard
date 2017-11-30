b05dd2e71924035e0218e0b098cf1ed7b194aadc
Karina Yang <karina.yang@deciphernow.com>
2017-11-30 15:42:18 -0500

    1110 fix flex layout (#1122)
    * fix flex layout
    
    * update snapshots

--- src/components/LayoutSection/components/LayoutSectionWrap.js
+++ src/components/LayoutSection/components/LayoutSectionWrap.js
@@ -6,6 +6,7 @@ import { COLOR_CONTENT } from "style/styleVariables";
 const LayoutSectionWrap = styled.section`
   margin-bottom: ${spacingScale(4)};
   display: flex;
+  flex: 0 0 auto;
   flex-direction: column;
   align-items: stretch;
   color: ${COLOR_CONTENT.string()};
