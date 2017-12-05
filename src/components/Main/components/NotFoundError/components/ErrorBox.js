9c4453205d5cbc2435c0fb30bf9cedb1377d1e45
Karina Yang <karina.yang@deciphernow.com>
2017-12-05 14:51:10 -0500

    fix errorboundary alignment (#1166)

--- src/components/Main/components/NotFoundError/components/ErrorBox.js
+++ src/components/Main/components/NotFoundError/components/ErrorBox.js
@@ -4,9 +4,10 @@ const ErrorBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
+  height: auto;
   user-select: none;
   cursor: default;
+  flex: 1;
 `;
 
 export default ErrorBox;
