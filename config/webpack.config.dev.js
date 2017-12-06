e2c37b1b41c1c1bd03b4fbdf9fb4341739aafe6a
David Tillery <tilleryd@gmail.com>
2017-12-06 15:30:06 -0500

    192 vendor bundle (#1177)
    * Extract vendor node modules into separate bundles.
    
    * added dygraphs alias to webebpack.config.dev
    
    * Adjusted config to ensure that static asset hashes only change when appropriate.

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -92,7 +92,8 @@ module.exports = {
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      "react-native": "react-native-web"
+      "react-native": "react-native-web",
+      dygraphs: paths.appNodeModules + "/dygraphs/src-es5/dygraph.js"
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
