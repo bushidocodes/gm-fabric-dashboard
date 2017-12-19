7194680574af51a191f4d63f20b0f53aaeacba5d
Kait Moreno <kaitlin.moreno@deciphernow.com>
2017-12-19 17:57:53 -0500

    test: added tests for dygraph utils (#1252)

--- src/utils/dygraphs.test.js
+++ src/utils/dygraphs.test.js
@@ -1,9 +1,49 @@
-import { floatRound } from "./dygraphs";
+import {
+  floatRound,
+  getDygraphOfValue,
+  mapDygraphKeysToNetChange
+} from "./dygraphs";
+import mock from "json/mockReduxStateMetrics";
 
-xdescribe("getDygraphOfValue", () => {
-  xtest("TODO", () => {});
+describe("getDygraphOfValue", () => {
+  test("returns time series data of one or more in Dygraph format", () => {
+    let metrics = mock.instance.metrics;
+    let keys = ["HTTPS/requests", "HTTP/requests"];
+    let labels = ["HTTPS", "HTTP"];
+
+    expect(getDygraphOfValue(metrics, keys, labels)).toEqual([
+      [
+        [new Date("2017-11-14T22:48:14.557Z"), 50, 1193388],
+        [new Date("2017-11-14T22:48:19.598Z"), 50, 1193388],
+        [new Date("2017-11-14T22:48:24.508Z"), 50, 1193388],
+        [new Date("2017-11-14T22:48:29.508Z"), 50, undefined],
+        [new Date("2017-11-14T22:48:34.507Z"), 50, undefined]
+      ],
+      { labels: ["time", "HTTPS", "HTTP"] }
+    ]);
+  });
 });
-xdescribe("mapDygraphKeysToNetChange", () => {});
+
+describe("mapDygraphKeysToNetChange", () => {
+  test("returns net change", () => {
+    const dygraphData = [
+      [
+        [new Date("Tue Dec 19 2017 14:03:16 GMT-0500 (EST)"), 1692],
+        [new Date("Tue Dec 19 2017 14:03:21 GMT-0500 (EST)"), 1700]
+      ],
+      { labels: ["time", "Requests Per Second"] }
+    ];
+    const labelsToMap = ["Requests Per Second"];
+
+    expect(mapDygraphKeysToNetChange(dygraphData, labelsToMap)).toEqual([
+      [
+        [new Date("2017-12-19T19:03:16.000Z"), 0],
+        [new Date("2017-12-19T19:03:21.000Z"), 1.6]
+      ],
+      { labels: ["time", "Requests Per Second"] }
+    ]);
+  });
+});
 
 describe("floatRound", () => {
   test("rounds a number to the number of decimal places specified", () => {
