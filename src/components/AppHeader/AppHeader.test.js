64971ece1b2d284f96128255b9261b17b1dcef8c
Kait Moreno <kaitlin.moreno@deciphernow.com>
2017-12-19 09:31:24 -0500

    Test: Add unit test for AppHeader component  (#1247)
    * test: added tests for AppHeader
    
    * test: added tests for AppHeader
    
    * test: delete unused vars
    
    * chore: added comments
    
    * chore: update comments

---
+++ src/components/AppHeader/AppHeader.test.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { mount, render } from "enzyme";
+import { MemoryRouter, Route } from "react-router";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+
+// utils
+import { getFabricServer } from "../../utils/head";
+import mockState from "json/mockReduxState";
+
+// components
+import AppHeader from "./AppHeader";
+import UseSDS from "./scenes/UseSDS";
+import NoSDS from "./scenes/NoSDS";
+
+// styled-components
+import AppHeaderContainer from "./components/AppHeaderContainer";
+
+const store = configureMockStore()(mockState);
+
+const anAppHeader = (
+  <Provider store={store}>
+    <MemoryRouter initialEntries={["/Down"]}>
+      <Route render={props => <AppHeader {...props} />} />
+    </MemoryRouter>
+  </Provider>
+);
+
+// mock getFabricServer
+jest.mock("../../utils/head");
+
+describe("AppHeader component", () => {
+  let AppHeaderWrapper;
+
+  beforeEach(() => {
+    AppHeaderWrapper = mount(anAppHeader);
+  });
+
+  test("matches snapshot with instance view tabs", () => {
+    AppHeaderWrapper = render(anAppHeader);
+    expect(AppHeaderWrapper).toMatchSnapshot();
+  });
+
+  test("matches snapshot with fabric view tabs", () => {
+    // set a return value for getFabricServer() util func so that AppHeader renders <UseSDS /> and remount
+    getFabricServer.mockImplementation(() => "http://localhost:1337");
+    AppHeaderWrapper = render(anAppHeader);
+    expect(AppHeaderWrapper).toMatchSnapshot();
+  });
+
+  test("renders subcomponents", () => {
+    expect(AppHeaderWrapper.find(AppHeaderContainer)).toHaveLength(1);
+    expect(AppHeaderWrapper.find("AppToolBar")).toHaveLength(1);
+    expect(AppHeaderWrapper.find("Banner")).toHaveLength(1);
+  });
+
+  test("renders <UseSDS /> when using a fabric server", () => {
+    expect(AppHeaderWrapper.find(UseSDS)).toHaveLength(1);
+    expect(AppHeaderWrapper.find(NoSDS)).toHaveLength(0);
+  });
+
+  test("renders <NoSDS /> when there is no fabric server", () => {
+    // set a return value for getFabricServer() util func so that AppHeader renders <NoSDS /> and remount
+    getFabricServer.mockImplementation(() => {});
+    AppHeaderWrapper = mount(anAppHeader);
+    expect(AppHeaderWrapper.find(NoSDS)).toHaveLength(1);
+    expect(AppHeaderWrapper.find(UseSDS)).toHaveLength(0);
+  });
+
+  test("passes the correct title to Banner", () => {
+    expect(AppHeaderWrapper.find("Banner").props().title).toBe("Down");
+  });
+
+  test("passes the pathname to AppToolBar", () => {
+    expect(AppHeaderWrapper.find("AppToolBar").props().pathname).toBe("/Down");
+  });
+});
