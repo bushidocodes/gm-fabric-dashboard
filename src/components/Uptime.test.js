89ed6db7e9971432a44e66de2a7272103a38dcdb
Kait Moreno <kaitlin.moreno@deciphernow.com>
2017-12-19 09:35:31 -0500

    test: add unit test for Uptime component (#1246)

---
+++ src/components/Uptime.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { mount } from "enzyme";
+import _ from "lodash";
+
+import Uptime from "./UpTime";
+import ArrayValue from "components/ArrayValue";
+
+const renderFunc = uptime => (
+  <ArrayValue>{_.map(uptime, el => <span key={el}>{el} </span>)}</ArrayValue>
+);
+
+const startTime = 1401164339200;
+
+// Mock the Date.now function
+Date.now = () => 1511164339200;
+
+// Tell jest to mock all timers
+jest.useFakeTimers();
+
+describe("Uptime component", () => {
+  let UptimeWrapper;
+  beforeEach(() => {
+    UptimeWrapper = mount(<Uptime startTime={startTime} render={renderFunc} />);
+  });
+
+  test("renders via the function passed as the render prop", () => {
+    expect(UptimeWrapper.find(ArrayValue)).toHaveLength(1);
+  });
+
+  test("renders the correct value", () => {
+    jest.runTimersToTime(1000);
+    expect(UptimeWrapper.state().uptime).toMatchObject([
+      "1273d",
+      "03h",
+      "33m",
+      "20s"
+    ]);
+    expect(UptimeWrapper.html().includes("1273d", "03h", "33m", "20s")).toBe(
+      true
+    );
+  });
+});
