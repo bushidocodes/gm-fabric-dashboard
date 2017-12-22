1177283e107441810cd76e5c9fef9f4e5a5ef8e7
Kait Moreno <kaitlin.moreno@deciphernow.com>
2017-12-22 08:50:50 -0500

    feat: add notfounderror to storybook (#1280)

---
+++ src/components/Main/components/NotFoundError/NotFoundError.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, text } from "@storybook/addon-knobs";
+
+import NotFoundError from "./NotFoundError";
+
+storiesOf("NotFoundError", module)
+  .addDecorator(withKnobs)
+  .add("default", () => (
+    <div style={{ display: "flex" }}>
+      <NotFoundError errorMsg={text("errorMsg")} />
+    </div>
+  ));
