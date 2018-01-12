a1485830d142dabbfaa3c0bd04f41e405d2f5fa8
Sean McBride <spmcbride1201@gmail.com>
2018-01-12 16:49:27 -0500

    refactor: use env var to set fabricServer in dev mode (#1354)

--- src/utils/head.js
+++ src/utils/head.js
@@ -2,12 +2,19 @@
 
 /**
  * getFabricServer is a utility function that extracts the fabricServer property
- * from the HEAD of the index.html file.
+ * from the HEAD of the index.html file and returns it if it's been set.
+ * Otherwise, if NODE_ENV is in development mode, it returns http://localhost:1337
  * @returns {String}
  */
 export function getFabricServer() {
   const fabricServer = document.head.querySelector("[property=fabricServer]")
     .content;
-
-  return fabricServer !== "__FABRIC_SERVER__" ? fabricServer : null;
+  // Return the metatag if set
+  if (fabricServer !== "__FABRIC_SERVER__") {
+    return fabricServer;
+  } else if (process.env.NODE_ENV === "development") {
+    return "http://localhost:1337";
+  } else {
+    return null;
+  }
 }
