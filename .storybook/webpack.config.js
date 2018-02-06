07feb8cd0b6ba29206d997381324aa8fc6b3879a
Sean McBride <spmcbride1201@gmail.com>
2018-02-06 10:10:22 -0500

    fix: storybook now imports css as expected (#1474)

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -59,23 +59,17 @@ module.exports = {
             loader: require.resolve("css-loader"),
             options: {
               importLoaders: 1,
-              modules: true,
               sourceMap: true
             }
           },
           {
             loader: require.resolve("postcss-loader"),
             options: {
+              ident: "postcss",
               sourceMap: true,
               plugins: () => [
                 require("postcss-flexbugs-fixes"),
                 autoprefixer({
-                  browsers: [
-                    ">1%",
-                    "last 4 versions",
-                    "Firefox ESR",
-                    "not ie < 9" // React doesn't support IE8 anyway
-                  ],
                   flexbox: "no-2009"
                 })
               ]
