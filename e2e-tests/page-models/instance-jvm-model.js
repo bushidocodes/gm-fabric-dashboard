baccd77d3bbda7364b3bdabb783a8e33be40711c
Kait Moreno <kaitlin.moreno@deciphernow.com>
2018-02-16 15:32:53 -0500

    feat: add instance-view selectors (#1528)
    * feat: add local testing script
    
    * feat: add base instance model
    
    * feat: add sort options to base instance model
    
    * fix: update explorer view selectors
    
    * feat: add instance jvm model
    
    * feat: add instance go model
    
    * feat: add jvm summary readout selector

---
+++ e2e-tests/page-models/instance-jvm-model.js
@@ -0,0 +1,66 @@
+import ReactSelector from "testcafe-react-selectors";
+import { Selector } from "testcafe";
+import BaseInstanceView from "./base-instance-model";
+
+export default class InstanceViewJVM extends BaseInstanceView {
+  constructor() {
+    super();
+    // Summary
+    this.summaryHostCPUReadout = ReactSelector(
+      "ReadoutDisplay ItemDisplay"
+    ).withText("Host CPU Cores");
+
+    // Navigation
+    this.linkThreads = ReactSelector("TabLink").withText("Threads");
+    this.textThreadsCount = ReactSelector("TabLink")
+      .withText("Threads")
+      .find("dd");
+    this.linkHTTP = ReactSelector("TabLink").withText("HTTP");
+    this.textDetailHTTP = ReactSelector("TabLink")
+      .withText("HTTP")
+      .find("dd");
+    this.linkJVM = ReactSelector("TabLink").withText("JVM");
+    this.textDetailJVM = ReactSelector("TabLink")
+      .withText("JVM")
+      .find("dd");
+    this.linkFinagle = ReactSelector("TabLink").withText("Finagle");
+    this.textDetailFinagle = ReactSelector("TabLink")
+      .withText("Finagle")
+      .find("dd");
+
+    // Threads Table Toolbar
+    this.inputSearchThreads = ReactSelector("ToolbarLeft input");
+    this.selectGroupThreads = Selector(".Select-control").withText("Group");
+    // this.selectGroupThreads needs to be selected first
+    // so that the following options are created in the DOM
+    this.optionGroupThreadsState = Selector(".Select-option").withText("State");
+    this.optionGroupThreadsNone = Selector(".Select-option").withText("None");
+
+    this.selectSortThreads = Selector(".Select-control").withText("Sort");
+    // this.selectSortThreads needs to be selected first
+    // so that the following options are created in the DOM
+    this.optionSortThreadsState = Selector(".Select-option").withText("State");
+    this.optionSortThreadsID = Selector(".Select-option").withText("ID");
+    this.optionSortThreadsName = Selector(".Select-option").withText("Name");
+
+    // Threads Table
+    this.threadsTableRows = ReactSelector("TableRow");
+    this.threadsStackTraces = ReactSelector("StackTrace");
+
+    // HTTP
+    this.httpGrid1 = Selector(".react-grid-item").nth(0);
+    this.httpGrid2 = Selector(".react-grid-item").nth(1);
+    this.httpGrid3 = Selector(".react-grid-item").nth(2);
+    this.httpGrid4 = Selector(".react-grid-item").nth(3);
+
+    // JVM
+    this.jvmGrid1 = Selector(".react-grid-item").nth(0);
+    this.jvmGrid2 = Selector(".react-grid-item").nth(1);
+
+    // Finagle
+    this.finagleGrid1 = Selector(".react-grid-item").nth(0);
+    this.finagleGrid2 = Selector(".react-grid-item").nth(1);
+    this.finagleGrid3 = Selector(".react-grid-item").nth(2);
+    this.finagleGrid4 = Selector(".react-grid-item").nth(3);
+  }
+}
--- e2e-tests/tests/sample.js
+++ e2e-tests/tests/sample.js
@@ -1,13 +1,24 @@
 import ReactSelector from "testcafe-react-selectors";
+import BaseInstanceView from "../page-models/base-instance-model";
 
 fixture`Getting Started`.page`http://localhost:3000`;
 
+let instanceView = new BaseInstanceView();
+
 test("My first test", async t => {
-  const searchInput = ReactSelector("Toolbar").find("input");
-  await t
-    .expect(searchInput.value)
-    .eql("", "input is empty")
-    .typeText(searchInput, "Grace Hopper")
-    .expect(searchInput.value)
-    .contains("G", 'input contains text "G"');
+  await t.click(ReactSelector("GMServiceCard").withText("Crashy"));
+  await t.click(
+    ReactSelector("TableCol")
+      .find("a")
+      .nth(0)
+  );
+  await t.wait(2000);
+  await t.click(instanceView.linkExplorer);
+
+  await t.typeText(instanceView.inputSearchMetrics, "r");
+
+  await t.click(instanceView.inspectorItems.nth(0));
+  await t.wait(3000);
+  await t.expect(await instanceView.inspectorGraph.count).eql(1);
+  await t.expect(instanceView.inspectorGraphTitleText.innerText).eql("Hey");
 });
