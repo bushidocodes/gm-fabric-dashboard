304cd3d20a0028c8ecfc815d08f21c961b1589d7
Kait Moreno <kaitlin.moreno@deciphernow.com>
2018-02-19 10:08:16 -0500

    feat: add settings view model (#1530)

--- e2e-tests/tests/sample.js
+++ e2e-tests/tests/sample.js
@@ -1,9 +1,10 @@
 import ReactSelector from "testcafe-react-selectors";
 import BaseInstanceViewModel from "../view-models/base-instance-view-model";
-
+import SettingsViewModel from "../view-models/settings-view-model";
 fixture`Getting Started`.page`http://localhost:3000`;
 
 let instanceView = new BaseInstanceViewModel();
+let settingsView = new SettingsViewModel();
 
 test("My first test", async t => {
   await t.click(ReactSelector("GMServiceCard").withText("Crashy"));
@@ -12,13 +13,28 @@ test("My first test", async t => {
       .find("a")
       .nth(0)
   );
+
+  await t.click(settingsView.linkSettings);
+
+  // await t
+  //   .typeText(settingsView.fabricInputSlider, "80")
+  //   .expect(settingsView.fabricInputSlider.value)
+  //   .eql("80");
+
   await t.wait(2000);
-  await t.click(instanceView.linkExplorer);
+  // await t.click(settingsView.instancePollingButton);
 
-  await t.typeText(instanceView.inputSearchMetrics, "r");
+  // await t.click(instanceView.inspectorItems.nth(0));
+  // await t.wait(3000);
+  await t.click(settingsView.clearCacheButton);
+  await t.wait(1000);
+  await t.click(settingsView.clearCacheModalCancelButton);
+  await t.wait(1000);
+  await t.click(settingsView.clearCacheButton);
+  await t.wait(1000);
+  await t.click(settingsView.clearCacheModalConfirmButton);
+  await t.wait(1000);
+  await t.expect(settingsView.cacheSizeText.innerText).eql("0 B");
 
-  await t.click(instanceView.inspectorItems.nth(0));
-  await t.wait(3000);
-  await t.expect(await instanceView.inspectorGraph.count).eql(1);
-  await t.expect(instanceView.inspectorGraphTitleText.innerText).eql("Hey");
+  // await t.expect(instanceView.inspectorGraphTitleText.innerText).eql("Hey");
 });
