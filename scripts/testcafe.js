711c78f33090a25bfd30c5507befe77c513bf68f
Kaitlin Moreno <kaitmore@gmail.com>
2018-02-22 11:56:00 -0500

    feat: add getRunningBrowserstackSessions helper

--- scripts/testcafe.js
+++ scripts/testcafe.js
@@ -1,5 +1,6 @@
 const createTestCafe = require("testcafe");
 const glob = require("glob-promise");
+const BrowserStack = require("browserstack");
 
 // Each sub array defines a batch of browserstack workers.
 // Our current plan allows for a max of 5 workers at a time,
@@ -21,6 +22,28 @@ const SUPPORTED_BROWSERS = [
   ]
 ];
 
+const browserStackCredentials = {
+  username: process.env.BROWSERSTACK_USERNAME,
+  password: process.env.BROWSERSTACK_PASSWORD
+};
+
+async function getFiles(globPattern) {
+  return await glob(globPattern)
+    .then(files => files)
+    .catch(e => console.error(e));
+}
+
+async function getRunningBrowserstackSessions() {
+  const client = BrowserStack.createClient(browserStackCredentials);
+  const workerStatus = await new Promise(function(resolve, reject) {
+    client.getApiStatus((error, workers) => {
+      if (error) reject(error);
+      else resolve(workers);
+    });
+  });
+  return workerStatus;
+}
+
 async function createTestCafeInstance(browser, files) {
   let testcafe;
   await createTestCafe()
@@ -29,7 +52,7 @@ async function createTestCafeInstance(browser, files) {
       return tc
         .createRunner()
         .startApp("npm start")
-        .src(files)
+        .src("e2e-tests/tests/service-view.js")
         .browsers(browser)
         .run();
     })
@@ -41,17 +64,20 @@ async function createTestCafeInstance(browser, files) {
 }
 
 async function startTests(browsers, createTestCafeInstance) {
+  // The testcafe node api does not accept glob patterns, so grab relevant test files using node-glob
   let files = await getFiles("e2e-tests/tests/*.js");
-  for (let i = 0; i < browsers.length; i++) {
-    await createTestCafeInstance(browsers[i], files);
+  // Check that there are no tests already running
+  let sessionInfo = await getRunningBrowserstackSessions();
+  if (sessionInfo.running_sessions !== 0) {
+    console.error(
+      "There are not enough available Browserstack workers to run these tests. Please cancel any running sessions from the Browserstack Automate dashboard and try again."
+    );
+  } else {
+    // Create a new testcafe instance for each batch of browsers
+    for (let i = 0; i < browsers.length; i++) {
+      await createTestCafeInstance(browsers[i], files);
+    }
   }
-}
-
-async function getFiles(globPattern) {
-  const files = await glob(globPattern)
-    .then(files => files)
-    .catch(e => console.error(e));
-  return files;
 }
 
 startTests(SUPPORTED_BROWSERS, createTestCafeInstance);
