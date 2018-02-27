1d1a9e701dbb16492116fc1b5d72485e5232194e
shanberg <shanberg@icloud.com>
2018-02-27 14:48:59 -0500

    feat: improved sparkline style

--- src/components/Main/components/SparklineCol.js
+++ src/components/Main/components/SparklineCol.js
@@ -1,13 +1,21 @@
-import { spacingScale } from "style/styleFunctions";
+import { spacingScale, contrastColor } from "style/styleFunctions";
+import {
+  BORDER_RADIUS_BASE,
+  COLOR_CONTENT_BACKGROUND
+} from "style/styleVariables";
 
 import TableCol from "./TableCol";
 
 const SparklineCol = TableCol.extend`
-  padding-top: ${spacingScale(0.5)};
-  padding-bottom: ${spacingScale(0.5)};
+  padding: 0;
+  height: ${spacingScale(3)};
+  position: relative;
+  display: flex;
 
   svg {
-    height: ${spacingScale(3)};
+    flex: 1 1 100%;
+    border-radius: ${BORDER_RADIUS_BASE};
+    background-color: rgba(144, 144, 144, 0.1);
   }
 `;
 
