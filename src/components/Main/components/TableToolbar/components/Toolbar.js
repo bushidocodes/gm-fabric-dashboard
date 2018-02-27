f144685eb7455903991b6859058a527c2f7ad7fa
shanberg <shanberg@icloud.com>
2018-02-27 14:50:20 -0500

    feat: improved toolbar style

--- src/components/Main/components/TableToolbar/components/Toolbar.js
+++ src/components/Main/components/TableToolbar/components/Toolbar.js
@@ -1,45 +1,19 @@
 import styled from "styled-components";
+import Color from "color";
 
-import {
-  COLOR_CONTENT_BACKGROUND,
-  CONTENT_MAX_WIDTH
-} from "style/styleVariables";
+import { COLOR_CONTENT_BACKGROUND, TABLE_BORDER } from "style/styleVariables";
 import { contrastColor, spacingScale } from "style/styleFunctions";
-
-const TOOLBAR_BACKGROUND_COLOR = contrastColor(COLOR_CONTENT_BACKGROUND, 0.04)
-  .hsl()
-  .string();
 
 const Toolbar = styled.div`
   align-items: center;
-  background-color: ${TOOLBAR_BACKGROUND_COLOR};
+  border-bottom: 1px solid ${TABLE_BORDER};
+  justify-content: center;
   display: flex;
   flex-direction: row;
-  height: ${spacingScale(4.5)};
-  margin-left: auto;
-  margin-right: auto;
+  height: ${spacingScale(5)};
   width: 100%;
-  max-width: ${CONTENT_MAX_WIDTH};
   padding: ${spacingScale(0.25)} ${spacingScale(2)};
-
-  @media all and (min-width: ${CONTENT_MAX_WIDTH}) {
-    &:after {
-      background-color: inherit;
-      content: "";
-      height: inherit;
-      left: 0;
-      pointer-events: none;
-      position: absolute;
-      right: 0;
-      top: 0;
-      z-index: 1;
-    }
-
-    > * {
-      position: relative;
-      z-index: 2;
-    }
-  }
+  margin: ${spacingScale(2)} 0 ${spacingScale(1)};
 `;
 
 export default Toolbar;
