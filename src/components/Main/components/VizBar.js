7d88aaa9f37a8ee9fbc2072f2641acebd105fbdf
Stuart Hanberg <shanberg@users.noreply.github.com>
2018-02-28 10:13:12 -0500

    feat: improved badge style (#1568)
    * feat: improved badge style
    
    * chore: straightening up css and snapshots
    
    * feat: improved sparkline style
    
    * chore: cleaning up unused imports
    
    * chore: updating snapshots
    
    * feat: improved vizbar style
    
    * chore: updating snapshots
    
    * chore: straightening up css and snapshots
    
    * fix: fixing incidental issue with vizfill color
    
    * chore: updating snapshots

--- src/components/Main/components/VizBar.js
+++ src/components/Main/components/VizBar.js
@@ -5,10 +5,10 @@ import { COLOR_CONTENT_BACKGROUND } from "style/styleVariables";
 
 const VizBar = styled.div`
   background-color: ${contrastColor(COLOR_CONTENT_BACKGROUND, 0.1).string()};
-  border-radius: 1px 4px 4px 1px;
+  border-radius: 2px;
   bottom: 0;
-  height: 3px;
-  left: 0;
+  height: 1px;
+  left: ${spacingScale(2)};
   position: absolute;
   right: ${spacingScale(2)};
 `;
